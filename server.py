fa00e18c99ffcfc3227bc768787b8b631e3ac49d
Michael <G00411301@gmit.ie>
2024-05-17 19:17:55 +0100

    Initial flask server established with just a home page

--- server.py
+++ server.py
@@ -1,3 +1,14 @@
 #This is the main server side file in the application and contains all of the restfulAPI code
 
 from flask import Flask
+
+app = Flask(__name__, static_url_path='', static_folder='.')
+
+@app.route('/')
+def home():
+    return "Testing setup - home"
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
