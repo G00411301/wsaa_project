2c5fc3b353e0de8291ab254864cebabac5922393
Michael <G00411301@gmit.ie>
2024-05-18 13:17:58 +0100

    Updating the contactcentre.html and beginning the create functionality

--- server.py
+++ server.py
@@ -5,21 +5,23 @@ from appDAO import appDAO
 
 app = Flask(__name__, static_url_path='', static_folder='.')
 
-@app.route('/allbooks')
-def allbooks():
-    results = appDAO.getcontacts()
-    return jsonify(results)
+#here we are creating a route that returns all of the contacts in the DB as json. This will be used for two functions
+# 1. when the route is called with a GET request, it will return with all of the rows in the data base, showing all of the contacts.
+# 2. When the route is called with a POST request, it will create a new record in the data base
+@app.route('/allcontacts', methods = ["GET", "POST"])
+def allcontacts():
+    if request.method == 'GET':
+        results = appDAO.getcontacts()
+        return jsonify(results)
+    elif request.method == 'POST':
+        pass
+    #TODO add create record functionality
 
-
+#here we are taking the id input in the URL and returng a contact with the matching ID
 @app.route('/single/<int:id>')
-def singlebook(id):
+def singlecontact(id):
     results = appDAO.findbyid(id)
     return jsonify(results)
-
-
-@app.route('/')
-def Home():
-    return rende
 
 
 
