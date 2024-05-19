8aea0f6866ddbd8b174d034512e61d5d05b5a4d9
Michael <G00411301@gmit.ie>
2024-05-19 13:04:46 +0100

    amended dbconfig.py and html for local server

--- dbconfig.py
+++ dbconfig.py
@@ -1,8 +1,8 @@
 #I added dbconfig.py to the git ignore file in order to stop the file being pushed to github as it contains the access details to the DB. This has been amended for project submission so the file can be cloned into python anywhere
 
 mysql = {
-    'host' : "localhost",
-    'user' : "root",
-    'password':"",
+    'host' : "G00411301.mysql.pythonanywhere-services.com",
+    'user' : "G00411301",
+    'password':"wsaa_project",
     'database':"mycontacts"
 }
