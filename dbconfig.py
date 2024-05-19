fdcafff7acd3327f1a79625fdba46c59a3d891ec
Michael <G00411301@gmit.ie>
2024-05-19 12:09:44 +0100

    amended git ignore to bring in dbconfig for deployment to python anywhere

---
+++ dbconfig.py
@@ -0,0 +1,8 @@
+#I added dbconfig.py to the git ignore file in order to stop the file being pushed to github as it contains the access details to the DB. This has been amended for project submission so the file can be cloned into python anywhere
+
+mysql = {
+    'host' : "localhost",
+    'user' : "root",
+    'password':"",
+    'database':"mycontacts"
+}
